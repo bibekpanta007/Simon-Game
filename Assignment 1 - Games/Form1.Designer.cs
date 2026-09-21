b4d12da5d0d5b44a749b2f3607e39e41da1dfc54
bibekpanta007 <bibekpanta183@gmail.com>
2026-09-21 11:04:55 -0600

    Initial commit

---
+++ Assignment 1 - Games/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Assignment_1___Games
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(64, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(600, 60);
+            label1.TabIndex = 0;
+            label1.Text = "SIMON";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(64, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(600, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Press START GAME to begin";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(23, 23, 23);
+            ClientSize = new Size(784, 919);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 10F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Simon";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Assignment 1 - Games/Form1.cs
@@ -0,0 +1,15 @@
+namespace Assignment_1___Games
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
